8ffe2b4e69d66c46d65fedb028eb402f080c27e7
LognCutMoi <longkakainfo@gmail.com>
2026-07-06 08:39:36 +0700

    1

--- frontend/src/app/pages/class-session/models/class-session-child.ts
+++
@@ -1,9 +0,0 @@
-import { InputSignal , ModelSignal } from '@angular/core';
-import { Class } from '@models/class';
-import { ClassSession } from '@models/class-session';
-
-export interface ClassSessionChild {
-	canChanges : InputSignal<boolean>
-	classObject : InputSignal<Class>,
-	classSession : ModelSignal<ClassSession>,
-}
